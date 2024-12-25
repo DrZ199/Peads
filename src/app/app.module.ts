5479b0ae7a156a99d392e57be6f02996445847d0
DrZ199 <190632761+DrZ199@users.noreply.github.com>
2024-12-26 00:37:57 +0500

    Peads/src/app/app.module.ts

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { AppComponent } from './app.component';
+import { ChatComponent } from './chat/chat.component';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ChatComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
