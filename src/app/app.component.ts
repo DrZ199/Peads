372b91e32c13780f762463af4901eb4b521531ce
DrZ199 <190632761+DrZ199@users.noreply.github.com>
2024-12-26 00:38:14 +0500

    Peads/src/app/app.component.ts

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<div class="container">
+               <h1>NelsonBot - Pediatric Knowledge Assistant</h1>
+               <app-chat></app-chat>
+             </div>`,
+  styles: []
+})
+export class AppComponent { }
